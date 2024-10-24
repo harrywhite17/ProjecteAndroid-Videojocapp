b57acbc285e8af335739be799888ab070a78bf17
Radostin <rivanov@iesebre.com>
2024-10-24 20:22:02 +0200

    the navbar is fixed and sticky

--- hivemind-project/src/App.jsx
+++ hivemind-project/src/App.jsx
@@ -1,5 +1,6 @@
-import { Disclosure, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import {Dialog, DialogPanel, Disclosure, Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/react'
 import {Bars3Icon, BellIcon, XMarkIcon} from '@heroicons/react/24/outline'
+import {useState} from "react";
 
 const navigation = [
     { name: 'Home', href: '#', current: true },
@@ -12,10 +13,10 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
-    return (
-        <Disclosure as="nav" className="bg-black/60 rounded-3xl mt-2 w-5/6 mx-auto backdrop-blur-2xl">
-            <div className="mx-auto max-w-full px-2 sm:px-6 lg:px-8">
+function NavigationBar(){
+    return(
+        <Disclosure as="nav" className="bg-black/60 rounded-3xl mt-2 w-5/6 mx-auto backdrop-blur-2xl sticky top-2">
+            <div className="mx-auto max-w-full px-2 sm:px-6 lg:px-8 ">
                 <div className="relative flex h-16 items-center justify-between">
 
                     {/* Mobile menu button and menu panel */}
@@ -158,6 +159,135 @@ export default function Example() {
                 </div>
             </Disclosure.Panel>
         </Disclosure>
+    );
+}
+
+function HeroSection(){
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    return(
+        <Disclosure as="div" className="flex flex-col items-center justify-center text-white">
+            <div className="bg-white">
+                <header className="absolute inset-x-0 top-0 z-50">
+                    <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                        <div className="fixed inset-0 z-50"/>
+                        <DialogPanel
+                            className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                            <div className="flex items-center justify-between">
+                                <a href="#" className="-m-1.5 p-1.5">
+                                    <span className="sr-only">Your Company</span>
+                                    <img
+                                        alt=""
+                                        src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                                        className="h-8 w-auto"
+                                    />
+                                </a>
+                                <button
+                                    type="button"
+                                    onClick={() => setMobileMenuOpen(false)}
+                                    className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                                >
+                                    <span className="sr-only">Close menu</span>
+                                    <XMarkIcon aria-hidden="true" className="h-6 w-6"/>
+                                </button>
+                            </div>
+                            <div className="mt-6 flow-root">
+                                <div className="-my-6 divide-y divide-gray-500/10">
+                                    <div className="space-y-2 py-6">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                    <div className="py-6">
+                                        <a
+                                            href="#"
+                                            className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                        >
+                                            Log in
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </DialogPanel>
+                    </Dialog>
+                </header>
+
+                <div className="relative isolate px-6 pt-14 lg:px-8">
+                    <div
+                        aria-hidden="true"
+                        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                    >
+                        <div
+                            style={{
+                                clipPath:
+                                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                            }}
+                            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                        />
+                    </div>
+                    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                            <div
+                                className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                                Announcing our next round of funding.{' '}
+                                <a href="#" className="font-semibold text-indigo-600">
+                                    <span aria-hidden="true" className="absolute inset-0"/>
+                                    Read more <span aria-hidden="true">&rarr;</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="text-center">
+                            <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+                                El futur dels videojocs està aquí
+                            </h1>
+                            <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+                                Nosaltres tenim la solució perfecta per a que puguis disfrutar del teu temps amb els amics
+                            </p>
+                            <div className="mt-10 flex items-center justify-center gap-x-6">
+                                <a
+                                    href="#"
+                                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    Get started
+                                </a>
+                                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                                    Registra&#39;t gratuïtament<span aria-hidden="true">→</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        aria-hidden="true"
+                        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+                    >
+                        <div
+                            style={{
+                                clipPath:
+                                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                            }}
+                            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                        />
+                    </div>
+                </div>
+            </div>
+        </Disclosure>
+    );
+}
+
+export default function Webpage() {
+    return (
+        <section>
+            <NavigationBar/>
+            <HeroSection />
+        </section>
     )
 }
+
+
 
