996074bfcde1d98e5d8e94537cb3fbbd195ba0f5
manelmendez1 <manelmendez@iesebre.com>
2024-10-28 19:40:45 +0100

    contingut Hero Section

--- hivemind-project/src/App.jsx
+++ hivemind-project/src/App.jsx
@@ -162,17 +162,18 @@ function NavigationBar(){
     );
 }
 
-function HeroSection(){
+function HeroSection() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-    return(
-        <Disclosure as="div" className="flex flex-col items-center justify-center text-white">
-            <div className="bg-white">
+    return (
+        <div className="relative flex flex-col items-center justify-center text-white">
+            <div className="hero-background" />
+            <div className="w-full h-full relative z-10">
                 <header className="absolute inset-x-0 top-0 z-50">
                     <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
                         <div className="fixed inset-0 z-50"/>
                         <DialogPanel
-                            className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                            className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-transparent px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                             <div className="flex items-center justify-between">
                                 <a href="#" className="-m-1.5 p-1.5">
                                     <span className="sr-only">Your Company</span>
@@ -218,19 +219,7 @@ function HeroSection(){
                     </Dialog>
                 </header>
 
-                <div className="relative isolate px-6 pt-14 lg:px-8">
-                    <div
-                        aria-hidden="true"
-                        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-                    >
-                        <div
-                            style={{
-                                clipPath:
-                                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                            }}
-                            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-                        />
-                    </div>
+                <div className="relative isolate px-6 pt-14 lg:px-8 w-full h-full">
                     <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
                         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                             <div
@@ -262,21 +251,9 @@ function HeroSection(){
                             </div>
                         </div>
                     </div>
-                    <div
-                        aria-hidden="true"
-                        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-                    >
-                        <div
-                            style={{
-                                clipPath:
-                                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                            }}
-                            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-                        />
-                    </div>
                 </div>
             </div>
-        </Disclosure>
+        </div>
     );
 }
 
