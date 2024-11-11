626fe0325a85872ce3dc93b0142e788242c2fc92
gerardmoreno123 <gerardmoreno@iesebre.com>
2024-11-11 19:22:59 +0100

    Create NewsletterSection

--- hivemind-project/src/components/newsletter.jsx
+++ hivemind-project/src/components/newsletter.jsx
@@ -7,14 +7,14 @@ function Newsletter() {
                 <div className="max-w-2xl mx-auto text-center">
                     <h2 className="text-white text-4xl font-extrabold">Subscribe to Our Newsletter</h2>
                     <div className="my-6">
-                        <p className="text-base text-gray-200">Subscribe to our newsletter and stay up to date with the
+                        <p className="text-base text-gray-200 pb-10">Subscribe to our newsletter and stay up to date with the
                             latest news, updates, and exclusive offers. Get valuable insights. Join our community today!</p>
                     </div>
 
                     <div
-                        className="max-w-2xl left-0 right-0 mx-auto w-full p-5 flex items-center shadow-lg absolute -bottom-10">
+                        className="max-w-2xl left-0 right-0 mx-auto w-full p-5 flex items-center bg-white/20 rounded shadow-lg absolute -bottom-14">
                         <input type="email" placeholder="Enter your email"
-                               className="w-full bg-red py-3.5 px-4 text-gray-800 text-base focus:outline-none"/>
+                               className="w-full bg-red py-3.5 px-4 text-gray-900 placeholder-gray-700 bg-white/20 text-base focus:outline-none"/>
                         <button
                             className="bg-[#a91079] hover:bg-[#a91079e2] text-white text-base font-semibold tracking-wide py-3.5 px-6 focus:outline-none">
                             Subscribe
