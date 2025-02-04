5cea7d397f23a982f1b7d3091aee18e247c9014f
Harry White <145098581+harrywhite17@users.noreply.github.com>
2025-02-04 17:03:18 +0100

    Delete Escriptori directory

--- Escriptori/Sprint2/resources/videos/show.blade.php
+++
@@ -1,9 +0,0 @@
-
-<x-videos-app-layout>
-    <div class="container">
-        <h1>{{ $video->title }}</h1>
-        <p>{{ $video->description }}</p>
-        <p>Published: {{ $video->formatted_published_at }}</p>
-        <video src="{{ $video->url }}" controls></video>
-    </div>
-</x-videos-app-layout>
